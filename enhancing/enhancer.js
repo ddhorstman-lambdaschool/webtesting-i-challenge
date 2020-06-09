83af5644c1b8a18713ff220e6b8e7e358eb9b849
David Horstman <davidzilla27@gmail.com>
2020-06-09 13:41:16 -0400

    Succeed tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,12 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return !item
+    ? null
+    : {
+        ...item,
+        enhancement: item.enhancement === 20 ? 20 : item.enhancement + 1,
+      };
 }
 
 function fail(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,6 +1,6 @@
 const enhancer = require("./enhancer.js");
 
-const initialItem = { durability: 0 };
+const initialItem = { durability: 50, enhancement: 19 };
 
 describe("enhancer", () => {
   describe("repair", () => {
@@ -8,8 +8,35 @@ describe("enhancer", () => {
       const repairedItem = enhancer.repair(initialItem);
       expect(repairedItem.durability).toBe(100);
     });
+    it("only modifies durability", () => {
+      const { durability, ...repairedItem } = enhancer.repair(initialItem);
+      const { durability: discard, ...initItem } = initialItem;
+      expect(repairedItem).toEqual(initItem);
+    });
     it("returns null if no input is supplied", () => {
       const returned = enhancer.repair();
+      expect(returned).toBe(null);
+    });
+  });
+  describe("success", () => {
+    it("doesn't modify an item's durability", () => {
+      const enhancedItem = enhancer.succeed(initialItem);
+      expect(enhancedItem.durability).toBe(initialItem.durability);
+    });
+    it("increases enhancement by the appropriate step", () => {
+      const enhancedOnce = enhancer.succeed(initialItem);
+      const enhancedTwice = enhancer.succeed(enhancedOnce);
+
+      expect(enhancedOnce.enhancement).toBe(20);
+      expect(enhancedTwice.enhancement).toBe(20);
+    });
+    it("only modifies enhancement", () => {
+      const { enhancement, ...enhancedItem } = enhancer.succeed(initialItem);
+      const { enhancement: discard, ...initItem } = initialItem;
+      expect(enhancedItem).toEqual(initItem);
+    });
+    it("returns null if no input is supplied", () => {
+      const returned = enhancer.succeed();
       expect(returned).toBe(null);
     });
   });
