549594c0ef43cca3cb344ec273b6b11b372afa65
David Horstman <davidzilla27@gmail.com>
2020-06-09 13:28:57 -0400

    Repair tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,9 +1,16 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
+
+const initialItem = { durability: 0 };
 
 describe("enhancer", () => {
   describe("repair", () => {
-    it("",()=>{
-      expect(true).toBe(true);
+    it("repairs an item that's passed in", () => {
+      const repairedItem = enhancer.repair(initialItem);
+      expect(repairedItem.durability).toBe(100);
     });
-  })
+    it("returns null if no input is supplied", () => {
+      const returned = enhancer.repair();
+      expect(returned).toBe(null);
+    });
+  });
 });
